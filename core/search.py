cd2c55cfd1241d5ae63e77ebf1a2e1ab832f1461
RequestAMirror <harleybenoit@gmail.com>
2019-08-11 13:15:10 -0600

    Update search.py

--- core/search.py
+++ core/search.py
@@ -19,7 +19,7 @@ class Search():
         return results
 
     def run(self):
-        for item in praw.models.util.stream_generator(lambda **kwargs: self.aio_stream(**kwargs)):
+        for item in praw.models.util.stream_generator(lambda **kwargs: self.aio_stream(**kwargs), skip_existing=True):
             if not self.db.find(item.author.id):
                 self.subreddit.banned.add(item.author)
                 self.db.add((item.author.id, item.author.name, time.time(), item.id,))
